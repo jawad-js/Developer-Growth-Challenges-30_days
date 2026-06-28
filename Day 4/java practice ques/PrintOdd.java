e156e39a166904b4d166fa0e471db0db3e33efac
Jawad Shaikh <8915.js@gmail.com>
2026-06-28 22:27:14 +0530

    practice java question

---
+++ Day 4/java practice ques/PrintOdd.java
@@ -0,0 +1,11 @@
+public class PrintOdd {
+    public static void main(String[] args) {
+        int number = 23;
+
+        if(number % 2 != 0) {
+            System.out.println("The number is odd: " + number);
+        } else {
+            System.out.println("The number is even: " + number);
+        }
+    }
+}
